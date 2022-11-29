494ef27103ee4f166b18ff67d4e88d529e25671d
nadazkia <nadazkia18@gmail.com>
2022-11-29 19:25:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const itemHouse = document.querySelectorAll('a')[0]
+const itemHeart = document.querySelectorAll('a')[1]
+const itemInfo = document.querySelectorAll('a')[2]
+const home = document.getElementById('home')
+const heart = document.getElementById('yours')
+const info = document.getElementById('info')
+
+// itemHeart.addEventListener('click', () => {
+//     home.remove()
+//     heart.classList.add('active')
+//     if (info.classList = 'active') {
+//         info.remove()
+//     }
+// })
+
+// itemInfo.addEventListener('click', () => {
+//     info.add()
+//     info.classList.add('active')
+//     heart.remove()
+// })
