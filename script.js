0ff6f184847b5900cf5a49b1fe240af9b812ed08
nadazkia <nadazkia18@gmail.com>
2022-12-06 08:54:28 +0700

    menambahkan animasi

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const itemHouse = document.querySelectorAll('a')[0]
-const itemHeart = document.querySelectorAll('a')[1]
+const itemYours = document.querySelectorAll('a')[1]
 const itemInfo = document.querySelectorAll('a')[2]
 const home = document.getElementById('home')
-const heart = document.getElementById('yours')
+const yours = document.getElementById('yours')
 const info = document.getElementById('info')
 
-// itemHeart.addEventListener('click', () => {
-//     home.remove()
-//     heart.classList.add('active')
-//     if (info.classList = 'active') {
-//         info.remove()
-//     }
-// })
 
-// itemInfo.addEventListener('click', () => {
-//     info.add()
-//     info.classList.add('active')
-//     heart.remove()
-// })
+itemYours.addEventListener('click', () => {
+    const yoursDiv = yours.querySelector('div');
+    yoursDiv.style.animation = 'munculTotal 1s ease-in-out 1';
+    let infoDiv = info.querySelector('div');
+    let infoP = infoDiv.querySelector('p');
+    let infoH1 = infoDiv.querySelector('h1');
+    infoP.style.animation = '';
+    infoH1.style.animation = '';
+    infoH1.style.animationDelay = ''
+})
+itemInfo.addEventListener('click', () => {
+    let infoDiv = info.querySelector('div');
+    let infoP = infoDiv.querySelector('p');
+    let infoH1 = infoDiv.querySelector('h1');
+    infoP.style.animation = 'munculTotal 1s ease-in-out 1';
+    infoH1.style.animation = 'munculTotal 1s ease-in-out 1, kedut 1s ease-in-out infinite alternate';
+    infoH1.style.animationDelay = '0s, 1s'
+    const yoursDiv = yours.querySelector('div');
+    yoursDiv.style.animation = '';
+})
