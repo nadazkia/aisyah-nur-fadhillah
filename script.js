3eee780148aa15f20597ccbdc23907436e5fc3ab
nadazkia <nadazkia18@gmail.com>
2022-12-07 13:33:45 +0700

    coba fullscreen

--- script.js
+++ script.js
@@ -173,3 +173,17 @@ video.addEventListener('mouseenter', () => {
 youtube.addEventListener('mouseenter', () => {
     audio.volume = '0.07'
 })
+
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+    if (element.requestFullScreen) {
+        element.requestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+    }
+}
+
+// Launch fullscreen for browsers that support it!
+launchFullScreen(document.documentElement); // the whole page
